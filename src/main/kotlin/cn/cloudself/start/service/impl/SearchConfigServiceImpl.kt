b02db9d6ddcca1519a4ff8e75f7b67468b3bdb93
HerbLuo <cloudself.cn@gmail.com>
2021-12-01 00:05:05 +0800

    查询方案

--- src/main/kotlin/cn/cloudself/start/service/impl/SearchConfigServiceImpl.kt
+++ src/main/kotlin/cn/cloudself/start/service/impl/SearchConfigServiceImpl.kt
@@ -1,8 +1,10 @@
 package cn.cloudself.start.service.impl
 
+import cn.cloudself.start.dao.SysSearchConfigQueryPro
 import cn.cloudself.start.dao.SysSearchConfigQueryProEx
 import cn.cloudself.start.dao.SysSearchUserPlanQueryPro
 import cn.cloudself.start.dao.SysSearchUserPlanQueryProEx
+import cn.cloudself.start.exception.http.RequestBadException
 import cn.cloudself.start.service.ISearchConfigService
 import cn.cloudself.start.util.WebUtil
 import org.springframework.stereotype.Service
@@ -10,12 +12,16 @@ import org.springframework.stereotype.Service
 @Service
 class SearchConfigServiceImpl: ISearchConfigService {
     override fun getPlan(planName: String) {
-        val id = WebUtil.getUserIdNonNull()
-        val userPlainList = SysSearchUserPlanQueryProEx
-            .leftJoinOn(SysSearchUserPlanQueryProEx.joiner().sysSearchConfigId(), SysSearchConfigQueryProEx.joiner().id())
-            .selectBy().sysUserId.equalsTo(id)
-            .andForeignField(SysSearchConfigQueryProEx.foreignField().name.equalsTo(planName))
+        val userId = WebUtil.getUserIdNonNull()
+        val searchConfigId = SysSearchConfigQueryPro.selectBy().name.equalsTo(planName).columnLimiter().id().firstOrNull()
+            ?: throw RequestBadException("找不到PlanName: ${planName}对应的查询方案配置")
+
+        val userPlainList = SysSearchUserPlanQueryPro
+            .selectBy().sysUserId.equalsTo(userId)
+            .and().sysSearchConfigId.equalsTo(searchConfigId)
             .run()
+
+        println(userPlainList)
 
     }
 }
