d588b60bccd650f92fa0832596906fe5df67c228
HerbLuo <cloudself.cn@gmail.com>
2022-07-25 23:50:34 +0800

    init

--- src/main/resources/sql_scripts/V2022.06.06.1.2__Init_Data_Query_User_Plan.sql
+++ src/main/resources/sql_scripts/V2022.06.06.1.2__Init_Data_Query_User_Plan.sql
@@ -1,14 +1,14 @@
 TRUNCATE TABLE sys_query_user_plan;
 
-INSERT INTO sys_query_user_plan (id, sys_user_id, sys_query_id, sys_query_tag_cn_redundant, name, public, status, create_by, create_time)
-VALUES (1, null, 2, '字典管理', '公共方案1', true, 'success', 'initializer', now());
-INSERT INTO sys_query_user_plan (id, sys_user_id, sys_query_id, sys_query_tag_cn_redundant, name, public, status, create_by, create_time)
-VALUES (2, null, 2, '字典管理', '公共方案2', true, 'success', 'initializer', now());
+INSERT INTO sys_query_user_plan (id, page_tag, sys_user_id, sys_query_id, sys_query_tag_cn_redundant, name, public, status, create_by, create_time)
+VALUES (1, 'page:zi_dian_guan_li', null, 2, '字典管理', '公共方案1', true, 'success', 'initializer', now());
+INSERT INTO sys_query_user_plan (id, page_tag, sys_user_id, sys_query_id, sys_query_tag_cn_redundant, name, public, status, create_by, create_time)
+VALUES (2, 'page:zi_dian_guan_li', null, 2, '字典管理', '公共方案2', true, 'success', 'initializer', now());
 
-INSERT INTO sys_query_user_plan (id, sys_user_id, sys_query_id, sys_query_tag_cn_redundant, name, status, create_by, create_time)
-VALUES (10000, 1, 2, '字典管理', '我的方案', 'success', 'initializer', now());
-INSERT INTO sys_query_user_plan (id, sys_user_id, sys_query_id, sys_query_tag_cn_redundant, name, status, create_by, create_time)
-VALUES (10001, 1, 2, '字典管理', '我的方案2', 'success', 'initializer', now());
+INSERT INTO sys_query_user_plan (id, page_tag, sys_user_id, sys_query_id, sys_query_tag_cn_redundant, name, status, create_by, create_time)
+VALUES (10000, 'page:zi_dian_guan_li', 1, 2, '字典管理', '我的方案', 'success', 'initializer', now());
+INSERT INTO sys_query_user_plan (id, page_tag, sys_user_id, sys_query_id, sys_query_tag_cn_redundant, name, status, create_by, create_time)
+VALUES (10001, 'page:zi_dian_guan_li', 1, 2, '字典管理', '我的方案2', 'success', 'initializer', now());
 
 TRUNCATE TABLE sys_query_user_plan_item;
 
